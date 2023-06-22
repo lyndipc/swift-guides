4e4f2e83e233878de66dee62fccf66d78a77b3b4
lyndipc <47511217+lyndipc@users.noreply.github.com>
2023-06-22 11:08:59 -0600

    Use giscus script

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -50,13 +50,13 @@ const siteMetadata = {
       metadata: '0',
       // theme example: light, dark, dark_dimmed, dark_high_contrast
       // transparent_dark, preferred_color_scheme, custom
-      theme: 'preferred_color_scheme',
+      theme: 'light',
       // Place the comment box above the comments. options: bottom, top
       inputPosition: 'bottom',
       // Choose the language giscus will be displayed in. options: en, es, zh-CN, zh-TW, ko, ja etc
       lang: 'en',
       // theme when dark mode
-      darkTheme: '',
+      darkTheme: 'transparent_dark',
       // If the theme option above is set to 'custom`
       // please provide a link below to your custom theme css file.
       // example: https://giscus.app/themes/custom_example.css
